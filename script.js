17e775c098b8e741ebb008c98425b472a08f0a84
Aleksei Gazov <lekha.gazov@yandex.ru>
2022-06-24 07:36:33 +0300

    fix: comments

--- script.js
+++ script.js
@@ -25,73 +25,98 @@ overlay.addEventListener('click', function() {
         }); 
 
 //Comments
-class TagsInput {
-    textInput;
-    buttonInput;
-    list;
-   
-    constructor() {
-        this.renderInput();
-        this.renderButton();
-        this.renderList();
-        this.renderComponent();
-       
-    }
+const revievsButton = document.querySelector('.revievs__button');
+const revievsText = document.querySelector('.revievs__text');
+const reviewsField = document.querySelector('.reviews__field');
+// const review = document.querySelector('.review');
 
-    renderInput() {
-        this.textInput = document.createElement('input')
-        this.textInput.type = "text";
-        this.textInput.className = 'text'
+
+
+
+revievsButton.addEventListener('click', function(e) {
+    const value = revievsText.value;  
+    text= document.createElement('p')
+    text.className = 'reviewNew'
+    revievsText.value = "";
+    text.append(value) 
+    reviewsField.append(text)
+})
+
+
+
+
+
+// class TagsInput {
+//     textInput;
+//     // buttonInput;
+//     // list;
+   
+//     constructor() {
+//         this.renderText();
+//         this.renderButton();
+//         // this.renderList();
+//         // this.renderComponent();
+       
+//     }
+
+//     renderText() {
+//         this.textInput = document.createElement('p')
+//         // this.textInput.type = "text";
+//         this.textInput.className = 'review'
           
 
-    }
-    renderButton() {
-        this.buttonInput = document.createElement('input')
-        this.buttonInput.type = 'button'
-        this.buttonInput.className = 'button'
-        this.buttonInput.value = "add new teg"
-        this.buttonInput.onclick = () => { this.buttonAddTegToLocalStorage(); this.remoteTextInput(); this.tagsDraw () }
+//     }
+//     renderButton() {
+//         const revievsButton = document.querySelector('.revievs__button');
+//         const revievsText = document.querySelector('.revievs__text');
+//         revievsButton.addEventListener('click', function(e) {
+//             const value = revievsText.value;  
+//             revievsText.value = "";
+//             reviewsField.append(value) 
+//         })
 
-    }
-    renderList() {
-        this.list = document.createElement('ol')
-        this.list.className = 'tags-area'
+//         //   this.buttonInput.onclick = () => { this.buttonAddTegToLocalStorage(); this.remoteTextInput(); this.tagsDraw () }
+
+//     }
+//     // renderList() {
+//     //     this.list = document.createElement('ol')
+//     //     this.list.className = 'tags-area'
        
-    }
+//     // }
   
-    renderComponent() {
-        let div = document.getElementById('component')
-        div.append(this.textInput)
-        document.getElementById('component').append(this.buttonInput)
-        document.getElementById('component').append(this.checkboxInput)
-        document.getElementById('component').append(this.list)
-    }
+//     // renderComponent() {
+//     //     let div = document.getElementById('component')
+//     //     div.append(this.textInput)
+//     //     document.getElementById('component').append(this.buttonInput)
+//     //     document.getElementById('component').append(this.checkboxInput)
+//     //     document.getElementById('component').append(this.list)
+//     // }
 
 
-    buttonAddTegToLocalStorage() {
-        const value = this.textInput.value;
-        const tagsStr = localStorage.getItem('input');
-        const tags = JSON.parse(tagsStr) || [];
-        tags.push(value)
-        localStorage.setItem('input', JSON.stringify(tags))
+//     // buttonAddTegToLocalStorage() {
+//     //     const value = this.textInput.value;
+//     //     const tagsStr = localStorage.getItem('input');
+//     //     const tags = JSON.parse(tagsStr) || [];
+//     //     tags.push(value)
+//     //     localStorage.setItem('input', JSON.stringify(tags))
        
-    }
-    remoteTextInput() {
-        this.textInput.value = ""
-    }
+//     // }
+//     // remoteTextInput() {
+//     //     this.textInput.value = ""
+//     // }
 
-    tagsDraw () {
-        const tegLi = document.createElement('li');
-        const tagsStr = localStorage.getItem('input');
-        const tags = JSON.parse(tagsStr);
-        const arrLi = []
-        tegLi.className = "new-teg";
-        tegLi.append(tags[tags.length - 1]);
-        console.log(tegLi)
-        this.list.append(tegLi)
-        arrLi.push(tegLi)
-        console.log(arrLi)
-    }
+//     // tagsDraw () {
+//     //     const tegLi = document.createElement('li');
+//     //     const tagsStr = localStorage.getItem('input');
+//     //     const tags = JSON.parse(tagsStr);
+//     //     const arrLi = []
+//     //     tegLi.className = "new-teg";
+//     //     tegLi.append(tags[tags.length - 1]);
+//     //     console.log(tegLi)
+//     //     this.list.append(tegLi)
+//     //     arrLi.push(tegLi)
+//     //     console.log(arrLi)
+//     // }
    
-}
-window.onload = () => { new TagsInput() }
+// }
+// window.onload = () => { new TagsInput() }
